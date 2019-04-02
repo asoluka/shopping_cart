6328085595e1ce560926d15f7c203bd1792d3c56
TC Asoluka <austinjerrytech@users.noreply.github.com>
2019-04-02 10:08:11 +0100

    Variable Sanitation

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 require_once("dbcontroller.php");
-$db_handle = new DBController();
+$db_handler = new DBController();
 if(!empty($_GET["action"])) {
 switch($_GET["action"]) {
 	case "add":
 		if(!empty($_POST["quantity"])) {
-			$productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
+			$productCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
 			$itemArray = array($productByCode[0]["code"]=>array('name'=>$productByCode[0]["name"], 'code'=>$productByCode[0]["code"], 'quantity'=>$_POST["quantity"], 'price'=>$productByCode[0]["price"], 'image'=>$productByCode[0]["image"]));
 			
 			if(!empty($_SESSION["cart_item"])) {
@@ -55,7 +55,7 @@ switch($_GET["action"]) {
 <a id="btnEmpty" href="index.php?action=empty">Empty Cart</a>
 <?php
 if(isset($_SESSION["cart_item"])){
-    $total_quantity = 0;
+    $total_qty = 0;
     $total_price = 0;
 ?>	
 <table class="tbl-cart" cellpadding="10" cellspacing="1">
